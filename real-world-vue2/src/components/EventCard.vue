b0a6660e0b804030f872aaabeadefa6377cf3014
benjamas <45569862+tanzbenjamas@users.noreply.github.com>
2021-03-13 15:37:01 +0700

    create eventList eventCreate eventShow

---
+++ real-world-vue2/src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h1 v-if="key" @click="handler(arg, event)"></h1>
+    <ul>
+        <li></li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            key: true
+        }
+    },
+
+}
+</script>
+<style>
+
+</style>
