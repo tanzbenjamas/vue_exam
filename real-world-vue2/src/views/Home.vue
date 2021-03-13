dfce6b6690fd6b92bfa5cc736055f211b643e0f8
benjamas <45569862+tanzbenjamas@users.noreply.github.com>
2021-03-13 18:18:01 +0700

    show craete list

--- real-world-vue2/src/views/Home.vue
+++ real-world-vue2/src/views/Home.vue
@@ -1,4 +1,14 @@
 <template>
   <div class="about">
+       <h1>Create List</h1>
+   <EventCard/>
   </div>
 </template>
+<script>
+import  EventCard  from "@/components/EventCard.vue";
+export default {
+components:{
+  EventCard
+}
+}
+</script>
