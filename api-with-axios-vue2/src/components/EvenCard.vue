2f893e4f5fdb456f6c2313380f042b9c64134151
benjamas <45569862+tanzbenjamas@users.noreply.github.com>
2021-03-13 19:36:22 +0700

    npm install -g json-server and json-server --watch db.json

--- api-with-axios-vue2/src/components/EvenCard.vue
+++ api-with-axios-vue2/src/components/EvenCard.vue
@@ -6,7 +6,21 @@
 
 <script>
 export default {
-
+// mock an api server
+data() {
+        return {
+          event: {
+            id: 1,
+            title: 'Beach Cleanup',
+            date: 'Tues Aug 19, 2018',
+            time: '6:00',
+            attendees: [
+              { id: 'abc123', name: 'Adam Jahr' },
+              { id: 'def456', name: 'Gregg Pollack' }
+            ]
+          }
+        }
+      }
 }
 </script>
 
